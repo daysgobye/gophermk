2f08ec22527e5bea7363b006475992cf903e4ecc
cole <smithcole@protonmail.com>
2023-02-17 22:29:15 -0800

    fs

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/daysgobye/gophermk
+
+go 1.20
+
+require (
+	tinygo.org/x/drivers v0.24.0 // indirect
+	tinygo.org/x/tinyfs v0.2.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"machine"
+	"time"
+
+	"github.com/daysgobye/gophermk/console"
+	"tinygo.org/x/drivers/flash"
+	"tinygo.org/x/tinyfs/fatfs"
+)
+
+var (
+	blockDevice = flash.NewQSPI(
+		machine.QSPI_CS,
+		machine.QSPI_SCK,
+		machine.QSPI_DATA0,
+		machine.QSPI_DATA1,
+		machine.QSPI_DATA2,
+		machine.QSPI_DATA3,
+	)
+
+	filesystem = fatfs.New(blockDevice)
+)
+
+func main() {
+
+	// Configure the flash device using the default auto-identifier function
+	config := &flash.DeviceConfig{Identifier: flash.DefaultDeviceIdentifier}
+	if err := blockDevice.Configure(config); err != nil {
+		for {
+			time.Sleep(5 * time.Second)
+			println("Config was not valid: "+err.Error(), "\r")
+		}
+	}
+
+	// Configure FATFS with sector size (must match value in ff.h - use 512)
+	filesystem.Configure(&fatfs.Config{
+		SectorSize: 512,
+	})
+
+	console.RunFor(blockDevice, filesystem)
+}
